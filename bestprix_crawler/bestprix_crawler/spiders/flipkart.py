49e8d4bbd692f2f9ab503405b749e52d91f554fb
Bhavsar Siddharth PC <siddharth2395@gmail.com>
2017-02-04 15:32:07 +0530

    crawling unique URLs

---
+++ bestprix_crawler/bestprix_crawler/spiders/flipkart.py
@@ -0,0 +1,22 @@
+import scrapy
+
+url_list=[]
+class flipkart(scrapy.Spider):
+	name = "flipkart"
+	start_urls = [
+		'https://www.flipkart.com/',
+	]
+	allowed_domains = ["flipkart.com"]
+	def parse(self, response):
+		global url_list
+		urls=response.xpath('//a/@href').extract()
+		for href in urls:
+			url=response.urljoin(href)
+			if url not in url_list:
+				url_list.append(url)
+				with open('/home/sid/Development/allurl.txt', 'ab') as f:
+					f.write(url+"\n")
+					f.close()
+				yield scrapy.Request(url, callback=self.parse)
+			else:
+				pass
