7c9d001cf5940bfdc8578d8ae6ca65216d55d74c
Bhavsar Siddharth Laptop <siddharth2395@gmail.com>
2017-03-03 20:11:52 +0530

    temp

--- bestprix_crawler/bestprix_crawler/spiders/infibeam.py
+++ bestprix_crawler/bestprix_crawler/spiders/infibeam.py
@@ -1,23 +1,39 @@
 import scrapy
+import MySQLdb
 
-url_list=set()
+#url_list=[]
 class flipkart(scrapy.Spider):
 	name = "infibeam"
 	start_urls = [
-		'https://www.infibeam.com/unboxed-phones/apple-iphone-6-unboxed/P-mobi-80108009355-cat-z.html#variantId=P-mobi-4516313840',
+		'https://www.infibeam.com/',
 	]
 	allowed_domains = ["infibeam.com"]
+
 	def db_ops(self, response):
+		name = response.xpath('//*[@id="title-mob"]/h1/text()').extract_first()
 		price = response.xpath('//*[@id="price-after-discount"]/span[2]/text()').extract_first()
+		price = int(price.replace(',', ''))
+		url = response.url
+		db = MySQLdb.connect("localhost","root","root","bestprix_db" )
+		cursor = db.cursor()
+		sql = "INSERT INTO web_app_infibeam (id, name, url, price) VALUES (NULL, '%s', '%s', %d)" % (name,url,price)
+		try:
+			cursor.execute(sql)
+			db.commit()
+		except:
+			db.rollback()
+		db.close()
+
 	def parse(self, response):
-		#global url_list
-		#urls=response.xpath('//a/@href').extract()
-		#for href in urls:
-		#	url=response.urljoin(href)
-		#	if url not in url_list:
-		#		url_list.add(url)
-		if response.xpath('//*[@id="price-after-discount"]/span[2]'):
-			self.db_ops(response)
-		#		yield scrapy.Request(url, callback=self.parse)
-		#	else:
-		#		pass
+#		global url_list
+		urls=response.xpath('//a/@href').extract()
+		for href in urls:
+			url=response.urljoin(href)
+#			if url not in url_list:
+#				url_list.append(url)
+			if response.xpath('//*[@id="price-after-discount"]/span[2]') and response.xpath('//*[@id="title-mob"]/h1'):
+				self.db_ops(response)
+			else:
+				yield scrapy.Request(url, callback=self.parse)
+			# else:
+			# 	pass
--- bestprix_website/web_app/admin.py
+++ bestprix_website/web_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import flipkart, infibeam
+
+admin.site.register(flipkart)
+admin.site.register(infibeam)
