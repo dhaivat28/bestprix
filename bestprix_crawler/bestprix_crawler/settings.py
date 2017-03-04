d70e3d07e585feafd3cd84802564fca85f0d7e71
Ubuntu <ubuntu@ip-172-31-15-32.ap-south-1.compute.internal>
2017-03-04 07:24:26 +0000

    infibeam sql file

--- bestprix_crawler/bestprix_crawler/settings.py
+++ bestprix_crawler/bestprix_crawler/settings.py
@@ -22,7 +22,7 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 1024
+CONCURRENT_REQUESTS = 1024
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
