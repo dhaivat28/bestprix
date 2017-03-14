09e5290f74bc16a2117a9337566aea1bb4c0d76d
Bhavsar Siddharth Laptop <siddharth2395@gmail.com>
2017-03-15 01:39:22 +0530

    amazon spider completed

--- bestprix_crawler/bestprix_crawler/spiders/amazon.py
+++ bestprix_crawler/bestprix_crawler/spiders/amazon.py
@@ -7,14 +7,7 @@ class flipkart(scrapy.Spider):
 	name = "amazon"
 	start_urls = [
 		 'http://www.amazon.in/',
-		# 'http://www.amazon.in/OnePlus-3T-Gunmetal-6GB-64GB/dp/B01FM8M0XE/ref=br_asw_pdt-1?pf_rd_m=A1VBAL9TL5WCBF&pf_rd_s=&pf_rd_r=0DW4QR2SCDGRXJ5BB8ZJ&pf_rd_t=36701&pf_rd_p=c80a0bb1-6a98-40cb-b035-91b6b9e0fbbf&pf_rd_i=desktop',
-		# 'http://www.amazon.in/Exclusive-Dummy-ASIN197/dp/B01NCN4ICO/ref=br_asw_pdt-2?pf_rd_m=A1VBAL9TL5WCBF&pf_rd_s=&pf_rd_r=0DW4QR2SCDGRXJ5BB8ZJ&pf_rd_t=36701&pf_rd_p=c80a0bb1-6a98-40cb-b035-91b6b9e0fbbf&pf_rd_i=desktop',
 	]
-	# with open('data/allurl_cat_test.txt', 'rb') as f:
-	# 	for line in f:
-	# 		start_urls.append(line)
-	# 	f.close()
-
 	allowed_domains = ["amazon.in"]
 	def start_requests(self):
 		for url in self.start_urls:
