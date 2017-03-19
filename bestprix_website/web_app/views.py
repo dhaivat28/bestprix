53bfcb42edb7879474b565df1b5404633412119d
Bhavsar Siddharth Laptop <siddharth2395@gmail.com>
2017-03-19 20:11:57 +0530

    search box created

--- bestprix_website/web_app/views.py
+++ bestprix_website/web_app/views.py
@@ -1,8 +1,14 @@
-#from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render
 from django.http import HttpResponse
-
+import sys, os, base64, datetime, hashlib, hmac
+import requests
+import urllib
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	return render(request, 'index.html')
+
+def search(request):
+	access_key = 'AKIAIBV3ZSCMXLX5DY6A'
+	secret_key = '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'
+
+	return HttpResponse("hello")
