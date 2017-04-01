876fb998f2870b65e890ebc81774c2d4354f8f56
Bhavsar Siddharth Laptop <siddharth2395@gmail.com>
2017-04-02 00:02:20 +0530

    user model created

--- bestprix_website/web_app/models.py
+++ bestprix_website/web_app/models.py
@@ -22,3 +22,10 @@ class amazon(models.Model):
 	name = models.CharField(max_length=200)
 	url = models.TextField(null=False)
 	price = models.IntegerField(null=False)
+
+class user_detail(models.Model):
+	email_id = models.CharField(max_length=100)
+	name = models.CharField(max_length=100)
+	password = models.CharField(max_length=100)
+	s_q = models.CharField(max_length=200)
+	s_a = models.CharField(max_length=100)
