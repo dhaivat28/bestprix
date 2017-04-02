8b102b4c898618e2083d4500106013a2f46a61fd
Bhavsar Siddharth Laptop <siddharth2395@gmail.com>
2017-04-02 20:05:14 +0530

    connection exception added

--- bestprix_website/web_app/views.py
+++ bestprix_website/web_app/views.py
@@ -90,50 +90,53 @@ def search(request):
 		amazon_request_url = amazon_signed_request('in',m_params,amazon_access_key,amazon_secret_key,'bestprix09-21')
 		print '\nBEGIN REQUEST====AMAZON=====>'
 		print '\nRequest URL = ' + amazon_request_url
-		amazon_r = requests.get(amazon_request_url)
-		print '\nAMAZON===>Response code: %d\n' % amazon_r.status_code
-		if amazon_r.status_code is 200:
-			root = lxml.etree.fromstring(amazon_r.text.encode('utf-8'))
-			items = root.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}Items')
-			item_set = items.findall('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}Item')
-			for item in item_set:
-				# for c in item :
-				# 	for a in item.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}OfferSummary'):
-				# 		print a
-				asin = item.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}ASIN')
-				product_url = item.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}DetailPageURL')
-				img = item.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}LargeImage')
-				offer_price = item.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}OfferSummary')
-				LowestNewPrice = offer_price.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}LowestNewPrice')
-				try:
-					list_price = LowestNewPrice.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}Amount')
-				except Exception:
-					list_price = None
-				if img is not None:
-					large_img_url = img.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}URL').text
-				else:
-					img_sets = item.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}ImageSets')
-					img_set = img_sets.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}ImageSet')
-					large = img_set.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}LargeImage')
-					t_img = large.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}URL')
-					# print
-					# for img_set in img_sets:
-					# 	for c in img_set:
-					# 		for cc in c:
-					# 			pass
-					# 			# print cc
-					large_img_url = t_img.text
-				# print img
-				for sub_item in item:
-					title = sub_item.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}Title')
-					# list_price = sub_item.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}ListPrice')
-					if title is not None and list_price is not None:
-						price = int(list_price.text)/100
-						# print price,"\t====>\t",title.text
-						amazon_set.append({'p_id':asin.text,'title':title.text,'price':price,'url':product_url.text,'img_url':large_img_url,'seller':'amazon','logo':'a.png'})
-			print "amazon product count:",len(amazon_set)
-		else:
-			print "invalid Response"
+		try:
+			amazon_r = requests.get(amazon_request_url)
+			print '\nAMAZON===>Response code: %d\n' % amazon_r.status_code
+			if amazon_r.status_code is 200:
+				root = lxml.etree.fromstring(amazon_r.text.encode('utf-8'))
+				items = root.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}Items')
+				item_set = items.findall('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}Item')
+				for item in item_set:
+					# for c in item :
+					# 	for a in item.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}OfferSummary'):
+					# 		print a
+					asin = item.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}ASIN')
+					product_url = item.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}DetailPageURL')
+					img = item.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}LargeImage')
+					offer_price = item.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}OfferSummary')
+					LowestNewPrice = offer_price.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}LowestNewPrice')
+					try:
+						list_price = LowestNewPrice.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}Amount')
+					except Exception:
+						list_price = None
+					if img is not None:
+						large_img_url = img.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}URL').text
+					else:
+						img_sets = item.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}ImageSets')
+						img_set = img_sets.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}ImageSet')
+						large = img_set.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}LargeImage')
+						t_img = large.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}URL')
+						# print
+						# for img_set in img_sets:
+						# 	for c in img_set:
+						# 		for cc in c:
+						# 			pass
+						# 			# print cc
+						large_img_url = t_img.text
+					# print img
+					for sub_item in item:
+						title = sub_item.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}Title')
+						# list_price = sub_item.find('{http://webservices.amazon.com/AWSECommerceService/2011-08-01}ListPrice')
+						if title is not None and list_price is not None:
+							price = int(list_price.text)/100
+							# print price,"\t====>\t",title.text
+							amazon_set.append({'p_id':asin.text,'title':title.text,'price':price,'url':product_url.text,'img_url':large_img_url,'seller':'amazon','logo':'a.png'})
+				print "amazon product count:",len(amazon_set)
+			else:
+				print "invalid Response"
+		except Exception:
+			print "\nStatus:Error sending request"
 		#flipkart block====================================================================================================>
 		print "_____________________________________________________________________________________________________________"
 		flipkart_aff_id='viraj2196'
@@ -144,34 +147,38 @@ def search(request):
 		}
 		flipkart_request_url='https://affiliate-api.flipkart.net/affiliate/search/json?query='+key+'&resultCount=10'
 		print '\nBEGIN REQUEST====FLIPKART=====>'
-		flipkart_r = requests.get(flipkart_request_url, headers=headers)
 		print '\nRequest URL = ' + flipkart_request_url
-		print '\nFLIPKART===>Response code: %d\n' % flipkart_r.status_code
-		if flipkart_r.status_code is 200:
-			jsonResponse=json.loads(flipkart_r.text)
-			# for p in jsonResponse["productInfoList"]:
-			# 	print p
-			# title = jsonResponse["productInfoList"][0]["productBaseInfo"]["productAttributes"]["title"]
-			# print title
-			# return render(request, 'search/index.html')
-			for i in xrange(0,len(jsonResponse["productInfoList"])):
-				p_id = jsonResponse["productInfoList"][i]["productBaseInfo"]["productIdentifier"]["productId"]
-				price = jsonResponse["productInfoList"][i]["productBaseInfo"]["productAttributes"]["sellingPrice"]["amount"]
-				title = jsonResponse["productInfoList"][i]["productBaseInfo"]["productAttributes"]["title"]
-				product_url = jsonResponse["productInfoList"][i]["productBaseInfo"]["productAttributes"]["productUrl"]
-				try:
-					img = jsonResponse["productInfoList"][i]["productBaseInfo"]["productAttributes"]["imageUrls"]["400x400"]
-				except:
-					t_img = jsonResponse["productInfoList"][i]["productBaseInfo"]["productAttributes"]["imageUrls"]
-					keys = t_img.keys()
-					img = t_img[keys[0]]
-				#print title,"---->",img
-				flipkart_set.append({'p_id':p_id,'title':str(title),'price':int(price),'url':product_url,'img_url':str(img),'seller':'flipkart','logo':"{% static 'images/sites/f.jpg' %}"})
-				# print "INR",jsonResponse["productInfoList"][i]["productBaseInfo"]["productAttributes"]["sellingPrice"]["amount"],"\t====>\t",jsonResponse["productInfoList"][i]["productBaseInfo"]["productAttributes"]["title"]
-			print "flipkart product count:",len(flipkart_set)
-			print "\n"
-		else:
-			print "invalid Response"
+		try:
+			flipkart_r = requests.get(flipkart_request_url, headers=headers)
+			print '\nFLIPKART===>Response code: %d\n' % flipkart_r.status_code
+			if flipkart_r.status_code is 200:
+				jsonResponse=json.loads(flipkart_r.text)
+				# for p in jsonResponse["productInfoList"]:
+				# 	print p
+				# title = jsonResponse["productInfoList"][0]["productBaseInfo"]["productAttributes"]["title"]
+				# print title
+				# return render(request, 'search/index.html')
+				for i in xrange(0,len(jsonResponse["productInfoList"])):
+					p_id = jsonResponse["productInfoList"][i]["productBaseInfo"]["productIdentifier"]["productId"]
+					price = jsonResponse["productInfoList"][i]["productBaseInfo"]["productAttributes"]["sellingPrice"]["amount"]
+					title = jsonResponse["productInfoList"][i]["productBaseInfo"]["productAttributes"]["title"]
+					product_url = jsonResponse["productInfoList"][i]["productBaseInfo"]["productAttributes"]["productUrl"]
+					try:
+						img = jsonResponse["productInfoList"][i]["productBaseInfo"]["productAttributes"]["imageUrls"]["400x400"]
+					except:
+						t_img = jsonResponse["productInfoList"][i]["productBaseInfo"]["productAttributes"]["imageUrls"]
+						keys = t_img.keys()
+						img = t_img[keys[0]]
+					#print title,"---->",img
+					flipkart_set.append({'p_id':p_id,'title':str(title),'price':int(price),'url':product_url,'img_url':str(img),'seller':'flipkart','logo':"{% static 'images/sites/f.jpg' %}"})
+					# print "INR",jsonResponse["productInfoList"][i]["productBaseInfo"]["productAttributes"]["sellingPrice"]["amount"],"\t====>\t",jsonResponse["productInfoList"][i]["productBaseInfo"]["productAttributes"]["title"]
+				print "flipkart product count:",len(flipkart_set)
+				print "\n"
+			else:
+				print "invalid Response"
+		except Exception:
+			print "\nStatus:Error sending request"
+
 		# product_set = amazon_set + flipkart_set
 		# sorted_product_set = sorted(product_set, key=lambda k: k['price'])
 		# for p in sorted_product_set:
